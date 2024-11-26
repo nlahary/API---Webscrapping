b18bb0edf663850513a23db2f9271cddecee2c64
nlahary <nathanaellahary@gmail.com>
2024-11-26 11:01:19 +0100

    feat(routes): redirect root route to swagger

--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -1,8 +1,14 @@
 """API Router for Fast API."""
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from src.api.routes import hello
 
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
+
+
+@router.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
