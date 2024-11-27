55c014b294e16c72b979b78be71a310a3f1ec500
nlahary <nathanaellahary@gmail.com>
2024-11-27 23:20:18 +0100

    feat(api): new endpoint /dataset

--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 
 from src.api.routes import hello
+from src.api.routes import data
 
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
+router.include_router(data.router, tags=["Data"])
 
 
 @router.get("/")
